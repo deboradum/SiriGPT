cbcc71f25b8d72061edd37b0b335582f2d44829b
deboradum <pvwpvw@protonmail.com>
2023-05-04 21:39:41 +0200

    Added whisper support

--- siriGPT.py
+++ siriGPT.py
@@ -25,8 +25,8 @@ def record(event, query):
     myrecording = sd.rec(int(rec_duration * rec_fs), samplerate=rec_fs, channels=1, blocking=True)  # Need to check channel
     wv.write("prompt.wav", myrecording, rec_fs, sampwidth=2)
 
-    # Whisper
-    # query[0] = whispered text
+    transcript = whisper.transcribe()
+    query[0] = transcript.text
 
     # Deletes recording file.
     if os.path.isfile("prompt.wav"):
@@ -41,11 +41,12 @@ def main():
     gpt_text = "\nWhat can I do for you?\n\t"
     try:
         while True:
+            prompt = ""
             prompt = input(gpt_text)
             if prompt == "":
-                query = [None]
+                queryT = [None]
                 e = threading.Event()
-                t = threading.Thread(target=record, args=(e, query), daemon=True)
+                t = threading.Thread(target=record, args=(e, queryT), daemon=True)
                 t.start()
                 # Progress bar/ wheel not working yet
                 while not e.is_set():
@@ -55,11 +56,15 @@ def main():
                     time.sleep(0.4)
                     print('\tRecording...', end="\r")
                     time.sleep(0.4)
+                prompt = queryT[0]
                 # Aanpassen aan return value enzo
-                if query[0] is None:
-                    print('\tDit was je ingesproken tekst', end="\r")
-            else:
-                pass
+                if prompt is None:
+                    print('\tError parsing prompt.', end="\r")
+                else:
+                    print(f'\t{prompt}', end="\r")
+
+            # Send prompt to GPT API
+
     # Catches ctrl+c & exits program.
     except KeyboardInterrupt:
         print("\nGoodbye.")
