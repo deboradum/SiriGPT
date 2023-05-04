457dfff19d9e956f5b2bdaa187ac1fa75d7e105b
deboradum <pvwpvw@protonmail.com>
2023-05-04 21:19:42 +0200

    comments

--- siriGPT.py
+++ siriGPT.py
@@ -1,3 +1,4 @@
+# Imports.
 import openai
 import os
 import sounddevice as sd
@@ -6,34 +7,44 @@ import threading
 import time
 import wavio as wv
 
+# Module files.
+import gpt
+import whisper
 
-fs = 44100
-duration = 5
 
+# Recording settings.
+rec_fs = 44100
+rec_duration = 6
+# Records prompt & sends to whisper API.
 def record(event, query):
+    # Deletes recording file if one exists.
     if os.path.isfile("prompt.wav"):
         os.remove("prompt.wav")
 
-    myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=1, blocking=True)
-    wv.write("prompt.wav", myrecording, fs, sampwidth=2)
+    # Records & saves prompt.
+    myrecording = sd.rec(int(rec_duration * rec_fs), samplerate=rec_fs, channels=1, blocking=True)  # Need to check channel
+    wv.write("prompt.wav", myrecording, rec_fs, sampwidth=2)
 
     # Whisper
     # query[0] = whispered text
 
+    # Deletes recording file.
     if os.path.isfile("prompt.wav"):
         os.remove("prompt.wav")
 
+    # Sets event to signal recording is over.
     event.set()
 
 
 def main():
+    # Initial text.
     gpt_text = "\nWhat can I do for you?\n\t"
     try:
         while True:
             prompt = input(gpt_text)
             if prompt == "":
-                e = threading.Event()
                 query = [None]
+                e = threading.Event()
                 t = threading.Thread(target=record, args=(e, query), daemon=True)
                 t.start()
                 # Progress bar/ wheel not working yet
@@ -49,6 +60,7 @@ def main():
                     print('\tDit was je ingesproken tekst', end="\r")
             else:
                 pass
+    # Catches ctrl+c & exits program.
     except KeyboardInterrupt:
         print("\nGoodbye.")
         exit()
